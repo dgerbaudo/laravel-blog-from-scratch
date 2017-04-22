c809cb10585e9d55849b56a0c4546251a79ac2e5
Daniel Gerbaudo <info@danielgerbaudo.com>
2017-04-22 11:25:45 -0300

    added lesson 13

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 
-//Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show');
 
 
 //
