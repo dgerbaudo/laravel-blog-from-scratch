0362d876ba0823236a52fc24d5bbf9cd09f5d516
Daniel Gerbaudo <info@danielgerbaudo.com>
2017-04-22 11:04:57 -0300

    added lesson 11

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -15,4 +15,32 @@ class PostsController extends Controller
     {
         return view('posts.show');
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+
+//        $post = new Post;
+//        $post->title = request('title');
+//        $post->body = request('body');
+//        $post->save();
+
+//        Post::create([
+//            'title' => request('title'),
+//            'body' => request('body')
+//        ]);
+
+        Post::create(request(['title', 'body']));
+
+
+        return redirect('/');
+
+
+
+    }
+
 }
