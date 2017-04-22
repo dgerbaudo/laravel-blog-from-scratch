de6a0c8da60038fef613e934409e3fea76419af2
Daniel Gerbaudo <info@danielgerbaudo.com>
2017-04-22 12:41:45 -0300

    added lesson 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,22 @@
     <div class="col-sm-8 blog-main">
         <h1>{{ $post->title }}</h1>
         {{ $post->body }}
+
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                    <li class="list-group-item">
+
+                        <strong> {{ $comment->created_at->diffForHumans() }}: &ensp;</strong>
+
+                        {{ $comment->body }}
+
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+
     </div>
 @endsection
